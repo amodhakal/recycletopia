3ac6a2ea55c074cd49cfe177e62073afbe4ec9a6
Amodh Dhakal <adhakal2@ncsu.edu>
2025-01-25 22:05:26 -0500

    Setup sqlite database in GORM

--- server/main.go
+++ server/main.go
@@ -5,11 +5,19 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 const PORT = ":8080"
+const SQLITE_DB = "recycletopia.db"
 
 func main() {
+	_, err := gorm.Open(sqlite.Open(SQLITE_DB), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Couldn't intialize GORM: ", err)
+	}
+
 	app := fiber.New()
 	app.Use(logger.New(logger.Config{Format: "${status} - ${method} ${path}\n"}))
 	log.Fatal(app.Listen(PORT))
